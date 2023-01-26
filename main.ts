208cb9a5c98de8f4643772e5fb9e6c9b999c262d
isabella709806 <122543587+isabella709806@users.noreply.github.com>
2023-01-26 09:26:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    testVariable += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    testVariable = 0
+})
+input.onButtonPressed(Button.B, function () {
+    testVariable += -1
+})
+let testVariable = 0
+testVariable = 0
