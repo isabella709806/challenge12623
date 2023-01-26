eabff696eb2ab97d6677820312a01e2daf7ad73a
isabella709806 <122543587+isabella709806@users.noreply.github.com>
2023-01-26 09:33:02 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     testVariable += 1
-    basic.showNumber(testVariable)
+    basic.showNumber(testVariable,50)
 })
 input.onButtonPressed(Button.AB, function () {
     testVariable = 0
-    basic.showNumber(testVariable)
+    basic.showNumber(testVariable,50)
 })
 input.onButtonPressed(Button.B, function () {
     testVariable += -1
-    basic.showNumber(testVariable)
+    basic.showNumber(testVariable,50)
 })
 let testVariable = 0
 testVariable = 0
-basic.showNumber(testVariable)
+basic.showNumber(testVariable,50)
